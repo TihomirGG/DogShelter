cec45d2400829a0bf570159c553d72d9b564f7df
Tihomir Gospodinov <tihomir.gospodinov@abv.bg>
2020-04-13 19:54:57 +0300

    fix

---
+++ DogShelter/Web/DogShelter.Web.ViewModels/Posts/PostWithPageCountViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DogShelter.Web.ViewModels.Posts
+{
+    using System.Collections.Generic;
+
+    public class PostWithPageCountViewModel
+    {
+        public int CurrentPage { get; set; }
+
+        public int PagesCount { get; set; }
+
+        public IEnumerable<PostsViewModel> Posts { get; set; }
+
+    }
+}
